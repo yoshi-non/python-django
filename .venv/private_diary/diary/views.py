2a5fabd2d6fa30df543c5416173c2781b5c4407e
yoshi-non <himekawa.yoshino44@gmail.com>
2022-08-22 00:37:17 +0900

    add 日記詳細表示機能の追加

--- .venv/private_diary/diary/views.py
+++ .venv/private_diary/diary/views.py
@@ -40,3 +40,8 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(
             user=self.request.user).order_by('-created_at')
         return diaries
+
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
