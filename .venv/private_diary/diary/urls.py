38f1adc367514d22c08a127e03835c27e13c8dff
yoshi-non <himekawa.yoshino44@gmail.com>
2022-08-22 04:17:55 +0900

    add 日記編集機能の追加

--- .venv/private_diary/diary/urls.py
+++ .venv/private_diary/diary/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('diary-detail/<int:pk>/',
          views.DiaryDetailView.as_view(), name="diary_detail"),
     path('diary-create/', views.DiaryCreateView.as_view(), name="diary_create"),
+    path('diary-update/<int:pk>',
+         views.DiaryUpdateView.as_view(), name="diary_update"),
 ]
