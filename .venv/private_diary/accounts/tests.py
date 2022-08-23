1942d4b0610acac2b88747526a90068d93007450
yoshi-non <himekawa.yoshino44@gmail.com>
2022-08-23 17:52:40 +0900

    add seleniumのテストコードの追加

--- .venv/private_diary/accounts/tests.py
+++ .venv/private_diary/accounts/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.urls import reverse_lazy
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-# Create your tests here.
+
+class TestLogin(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(
+            executable_path="C:/Users/TACHI/chromedriver.exe")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        # ログインページを開く
+        self.selenium.get("https://localhost:8000" +
+                          str(reverse_lazy("account_login")))
+
+        # ログイン
+        username_input = self.selenium.find_element_by_name("login")
+        username_input.send_keys("<ユーザー登録済みのメールアドレス>")
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys("<ログインパスワード>")
+        self.selenium.find_element_by_class_name("btn").click()
+
+        # ページタイトルの検証
+        self.assertEquals("日記一覧 | Private Diary", self.selenium.title)
